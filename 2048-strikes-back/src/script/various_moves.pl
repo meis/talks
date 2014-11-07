2c97e819b6b20e87c12cd015d4c72eb74af22372
Marc Egea i Sala <marc.egea.sala@gmail.com>
2014-11-07 12:44:38 +0100

    Split test scripts into one or various.

---
+++ 2048-strikes-back/src/script/various_moves.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use 5.012;
+
+use FindBin;
+use lib "lib";
+use Module::Load;
+
+my $class = $ENV{'CLASS'} || 'Solver';
+load $class;
+my $g = $class->new();
+
+$g->moves(1000);
+
+$g->best_move(
+    0, 0, 0, 0,
+    1, 0, 0, 0,
+    1, 0, 0, 0,
+    0, 0, 0, 0,
+);
+
+$g->best_move(
+    1, 2, 1, 2,
+    2, 1, 2, 1,
+    1, 2, 1, 0,
+    3, 3, 3, 3,
+);
+
+$g->best_move(
+    1, 3, 1, 2,
+    2, 3, 2, 1,
+    1, 2, 1, 2,
+    2, 1, 2, 1,
+);
+
+$g->best_move(
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    3, 2, 8, 2,
+    1, 3, 6, 4,
+);
